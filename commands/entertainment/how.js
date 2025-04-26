e36f93afe8290864cf28883ac1d253ddac94411c
Kyluxx <rizkypratamaikok@gmail.com>
2025-04-26 01:54:49 -0700

    Added how ...

---
+++ commands/entertainment/how.js
@@ -0,0 +1,25 @@
+`
+${command} *${text}*
+*${text}* is *${(101).getRandom()}*% ${command.replace('how', '').toUpperCase()}
+`
+
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+
+module.exports = { 
+    name: "how", 
+    aliases: ["segimana"], 
+    category: "entertainment",
+    permissions: { 
+        coin: 5
+    },
+    code: async (ctx) => { 
+        const types = ctx.args[0] || null;
+        const input = ctx.args[1] || null;
+
+        return await ctx.reply(quote(`Pertanyaan : how ${types} is *${input}*
+> *${input}* is *${(101).getRandom()}*% ${types.toUpperCase()}
+        ` + '\n \n' + config.msg.footer));
+    }
+};

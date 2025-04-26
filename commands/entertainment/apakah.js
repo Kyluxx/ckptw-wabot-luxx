3ac10e43baf372d1a7bf45ad5724af16f2b2187f
Kyluxx <rizkypratamaikok@gmail.com>
2025-04-26 01:29:05 -0700

    Added apakah

--- commands/entertainment/apakah.js
+++ commands/entertainment/apakah.js
@@ -67,6 +67,6 @@ module.exports = {
             'Nggak yakin banget',
             'Serius lo?',
             'Gajelas'
-        ])}` + config.msg.footer));
+        ])}` + '\n \n' + config.msg.footer));
     }
 };

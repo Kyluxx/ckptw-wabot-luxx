78b1fc8439c66946a8c7a5e48f1d7f6f3f095231
Kyluxx <rizkypratamaikok@gmail.com>
2025-04-27 01:46:19 +0700

    Added senna ai

---
+++ commands/ai-chat/senna.js
@@ -0,0 +1,23 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+
+const { default: axios } = require("axios");
+
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || null;
+
+        if (!input) return await ctx.quote(
+            `${quote(tools.cmd.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.cmd.generateCommandExample(ctx.used, "apa itu bot whatsapp?"))
+        );
+
+        try {
+            const apiUrl = "https://www.archive-ui.biz.id/api/ai/sennayapping?text=%2F*+balas+dengan+bahasa+indonesia+*%2F++"
+            const response = await axios.get(apiUrl + input).result
+
+            return await ctx.quote(response);
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
